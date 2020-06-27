4e76e6f4d6922efc912b6634d24a329e857e3838
jacken <jackenliy@gmail.com>
2020-06-27 21:05:29 +0800

    完成开发‘

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const config = {
     HOST: 'localhost',
     PORT: '3306',
     USER: 'root',
-    PASSWORD: '******',
+    PASSWORD: 'zj123456',
     DATABASE: 'dada_map',
   },
   jwt: {
     secret: '507f191e810c19729de860ea',
     options: {
       algorithm: 'HS256',
-      expiresIn: '2h'
-    }
-  }
+      expiresIn: '2h',
+    },
+  },
 };
 
 module.exports = config;
